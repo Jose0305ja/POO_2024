3305fb7cc61d96e20ca7cc51d4acddf5ccd19641
Jose0305ja <arr2005ja@gmail.com>
2024-05-21 13:08:23 -0600

    si

--- Parcial1/6_Ejercicios_Repaso/Ejercicio_10.py
+++ Parcial1/6_Ejercicios_Repaso/Ejercicio_10.py
@@ -1,9 +1,12 @@
-calificacion=[]
 i=1
-z=1
-for i in range (1,3):
-    calificacion=calificacion.add (input(f"Ingrese la calicacion del alumno {i}"))
+a=0
+r=0
+for i in range (1,16):
+    calificacion=int(input(f"Ingrese la calificacion del alumno {i}: "))
 
-for i in range (1,3):
-    calificacion[z]
-    z+=1
+    if calificacion <=100 and calificacion>=80:
+        a+=1
+    elif calificacion <80 and calificacion>=0:
+        r+=1
+print(f"Los alumnos aprobados son: {a}")
+print(f"Los alumnos reprobados son: {r}")
--- Parcial1/6_Ejercicios_Repaso/Ejercicio_7.py
+++ Parcial1/6_Ejercicios_Repaso/Ejercicio_7.py
@@ -4,8 +4,8 @@
 num_1=int(input("Ingrese el primer numero: "))
 num_2=int(input("Ingrese el segundo numero: "))
 
-for i in range (num_1+1, num_2):
-    num_1+=1
-    divi = num_1 % 2;
-    if divi==0:
-        print (num_1);
+for i in range (num_1, num_2):
+    divi = i % 2;
+    if divi==1:
+        print (i);
+    i+=1
