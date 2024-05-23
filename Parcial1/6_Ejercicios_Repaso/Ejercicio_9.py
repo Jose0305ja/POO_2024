15b2343c5495ee5473505ea7cda37aa435ecade5
Jose0305ja <arr2005ja@gmail.com>
2024-05-23 09:36:46 -0600

    parcial1

--- Parcial1/6_Ejercicios_Repaso/Ejercicio_9.py
+++ Parcial1/6_Ejercicios_Repaso/Ejercicio_9.py
@@ -1,4 +1,6 @@
-#Hacer un programa que solicite numeros indefinidamente hasta que se introduzca el 111 y salir del programa
+#Hacer un programa que solicite numeros indefinidamente 
+#hasta que se introduzca el 111 y salir del programa
+
 num=0
 print("Ingrese numero hasta que introduzca el correcto")
 while num!=111:
